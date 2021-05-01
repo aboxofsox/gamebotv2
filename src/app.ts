1bc44177f1ea6e17c5f473963ce28067b9941b81
aboxofsox <idkwthfml@gmail.com>
2021-05-01 12:38:12 -0500

    remove comments

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,3 @@
-/**
- *  //todo: add checks for empty, null, or invalid API calls
- *  todo: add error embed to handle errorful messages
- *  //todo: add support for image_additional
- *  todo: unique server instances with configurable questions
- *  todo: LFG
- *  //todo: top rated games
- */
-
-
 import * as dotenv from 'dotenv';
 import Main from './core/Main';
 dotenv.config();
