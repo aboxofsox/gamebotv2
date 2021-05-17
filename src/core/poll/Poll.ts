19b8e2254144863f9c03a6f13af92d9958210ac6
aboxofsox <idkwthfml@gmail.com>
2021-05-17 03:40:49 -0500

    added poll command

---
+++ src/core/poll/Poll.ts
@@ -0,0 +1,11 @@
+import {Interaction, CommandInteraction, Message} from 'discord.js';
+import Embedder from '../embedder/Embedder';
+
+
+
+export default class Poll {
+    static start(options: any, int: CommandInteraction, msg: Message) {
+        let max = options.length;
+
+    }
+}
